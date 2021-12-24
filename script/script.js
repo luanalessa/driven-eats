e4a794eb80ef94af303bee56c9276f2bb52ab0ef
Luana Lessa <lessalsn@gmail.com>
2021-12-24 01:39:30 -0300

    feat: Add order button

--- script/script.js
+++ script/script.js
@@ -1,1 +1,34 @@
-const order = {}
+const order = {};
+
+const options = document.querySelectorAll("li");
+const submit = document.querySelector(".submit-order");
+
+options.forEach((li) => {
+    li.addEventListener("click", () => {
+
+        userChoice = li.querySelector("label").innerText;
+
+        if ( order[li.parentNode.id] == null ){
+            order[li.parentNode.id] = userChoice;
+            li.classList.add("selected");
+            li.querySelector(".select-icon").style.display = "flex";
+        }
+        else if( userChoice == order[li.parentNode.id] ){
+            delete order[li.parentNode.id];
+            li.classList.remove("selected");
+            li.querySelector(".select-icon").style.display = "none";
+        } 
+
+        if(Object.keys(order).length == 3){
+            submit.classList.add("request");
+            submit.innerText = "Fechar pedido";
+        }
+        else{
+            submit.classList.remove("request");
+            submit.innerText = "Selecione os 3 itens para fechar o pedido";
+        }
+    });
+});
+
+submit.addEventListener("click", () => {
+})
